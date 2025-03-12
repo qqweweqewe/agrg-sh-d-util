8b71cc35faad7a296cf563e6104b430053a78812
qqweweqewe <golubkovivan4@gmail.com>
2025-03-12 18:57:33 +0300

    attempt number 4 (yayyy good stuffs)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod utils;
 
 use iced::{
-    widget::{text, button, column, row},
-    Sandbox, Settings,
+    widget::{text, button, column, row, pick_list},
+    Sandbox, Settings, Alignment,
 };
+use utils::get_available_ports;
 
 fn main() -> iced::Result {
     Agrg::run(Settings::default())
@@ -21,11 +22,13 @@ enum AgrgMsg {
     DoNothing,
     SettingsTab,
     JournalTab,
+    SerialChoice(String),
     CardsTab,
 }
 
 struct Agrg {
     tab: Tab,
+    port: Option<String>,
 }
 
 impl Sandbox for Agrg {
@@ -33,7 +36,8 @@ impl Sandbox for Agrg {
 
     fn new() -> Self {
         Self {
-            tab: Tab::Journal
+            tab: Tab::Journal,
+            port: Some(String::from("COM0")),
         }
     }
 
@@ -41,22 +45,37 @@ impl Sandbox for Agrg {
         "AGRG SH-D Utility".into()
     }
 
+    fn theme(&self) -> iced::Theme {
+        iced::Theme::GruvboxDark
+    }
+
     fn update(&mut self, message: Self::Message) {
-        self.tab = match message {
-            AgrgMsg::CardsTab => Tab::Cards,
-            AgrgMsg::JournalTab => Tab::Journal,
-            AgrgMsg::SettingsTab => Tab::Settings,
-            AgrgMsg::DoNothing => self.tab,
+        match message {
+            AgrgMsg::CardsTab => self.tab = Tab::Cards,
+            AgrgMsg::JournalTab => self.tab = Tab::Journal,
+            AgrgMsg::SettingsTab => self.tab = Tab::Settings,
+            AgrgMsg::SerialChoice(s) => self.port = Some(s), 
+            AgrgMsg::DoNothing => {},
         }
     } 
 
     fn view(&self) -> iced::Element<Self::Message> {
         column![
+            pick_list(
+                get_available_ports().expect("No Available ports")
+                .into_iter()
+                .map(|port| Some(port.port_name))
+                .collect(),
+                None,
+                |choice| AgrgMsg::SerialChoice(choice)
+            ).placeholder("Ports Selection"),
+
             row![
                 button("Journal").on_press(AgrgMsg::JournalTab),
                 button("Cards").on_press(AgrgMsg::CardsTab),
                 button("Settings").on_press(AgrgMsg::SettingsTab)
-            ],
+            ].align_items(Alignment::Center),
+            
             match self.tab {
                 Tab::Journal => {
                     text("Journal here")
