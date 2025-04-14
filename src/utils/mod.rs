ea9fbdfc5922fcb60e00c1fbd93b3cbcf871415c
qqweweqewe <golubkovivan4@gmail.com>
2025-04-14 10:25:19 +0300

    oops

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -163,7 +163,7 @@ pub fn mem_dump() -> Result<Vec<u8>, Box<dyn Error>> {
 
         let mut rx_part = atomic_serial_exchange(command)?;
         println!("Read 32 bytes from {:04X}", base_addr);
-        if (base_addr >= 0x1000 && rx_part == vec![0xff; 64]) || (rx_part) == vec![] {
+        if (base_addr >= 0x1000 && rx_part == vec![0xff; 32]) || (rx_part) == vec![] {
             return Ok(rx_vec);
         };
         rx_vec.append(&mut rx_part);
