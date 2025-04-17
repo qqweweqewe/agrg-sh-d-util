9ef9092e30ca3c82dda001e52e979b75a3e40d69
qqweweqewe <golubkovivan4@gmail.com>
2025-04-17 18:06:05 +0300

    added build thing

--- build.rs
+++ build.rs
@@ -2,6 +2,9 @@ fn main() {
     if std::env::var("CARGO_CFG_WINDOWS").is_ok() {
         let mut res = winres::WindowsResource::new();
         res.set_icon("icon.ico");
+        res.set("CompanyName", "ООО Агрегатор"); // Sets manufacturer
+        res.set("ProductName", "AGRG SH-D Utility");
+        res.set("FileDescription", "Утилита для настройки ручки AGRG SH-D");
         res.compile().expect("Failed to embed icon");
     }
 }
